7fb8d856a6e9b8b5c0ab6a1bc9d534b6484832ba
rohan agarwal <agarwalrohan189@gmail.com>
2020-08-09 00:40:10 +0530

    Create 1334B.cpp
    Middle Class

---
+++ 1334B.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>    
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define pb  push_back
+#define show(x) cerr<<(#x)<<" : "<<x<<endl;
+#define ll  long long
+#define ld  long double
+#define fill(a,val) memset(a,val,sizeof(a))
+#define mp  make_pair
+#define ff  first
+#define ss  second
+#define pii pair<ll,ll>
+#define sq(x) ((x)*(x))
+#define all(v) v.begin(),v.end()
+#define endl "\n"
+#define fo(i,n) for(int i=0;i<n;i++)
+#define foo(i,x,n) for(int i=x;i<n;i++)
+const ll MOD   = 1000*1000*1000+7;
+const ll MOD2  = 998244353;
+const ll N = 1000*1000+5;
+const double PI  = 3.14159265;
+ll power(ll x,ll y,ll p){ll res=1;x%=p;while(y>0){if(y&1)res=(res*x)%p;y=y>>1;x=(x*x)%p;}return res;}
+ 
+bool comp(double a,double b){
+	return a>b;
+}
+ 
+int main(){
+	fastio();
+	int t;
+	cin>>t;
+	while(t--){
+		int n,x;
+		cin>>n>>x;
+		double a[n];
+		double sum=0;
+		int count=0;
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+			sum+=a[i];
+		}
+		sort(a,a+n,comp);
+		for(int i=n-1;i>=0;i--){
+			if((sum/(i+1))>=x){
+				count+=(i+1);
+				break;
+			}
+			sum-=a[i];
+		}
+		cout<<count<<endl;
+	}
+}
