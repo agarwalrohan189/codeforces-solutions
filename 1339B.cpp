dac1bedc4bfc1f6e8ce156f60ee8dcca4fdabc84
rohan agarwal <agarwalrohan189@gmail.com>
2020-08-09 00:55:01 +0530

    633(B) Sorting Adjacent Differences

---
+++ 1339B.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>    
+using namespace std;
+#define fastio() ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define pb  push_back
+#define show(x) cerr<<(#x)<<" : "<<x<<endl;
+#define ll  long long
+#define ld  long double
+#define fill(a,val) memset(a,val,sizeof(a))
+#define mp  make_pair
+#define ff  first
+#define ss  second
+#define pii pair<ll,ll>
+#define sq(x) ((x)*(x))
+#define all(v) v.begin(),v.end()
+#define endl "\n"
+#define fo(i,n) for(int i=0;i<n;i++)
+#define foo(i,x,n) for(int i=x;i<n;i++)
+const ll MOD   = 1000*1000*1000+7;
+const ll MOD2  = 998244353;
+const ll N = 1000*1000+5;
+const double PI  = 3.14159265;
+ll power(ll x,ll y,ll p){ll res=1;x%=p;while(y>0){if(y&1)res=(res*x)%p;y=y>>1;x=(x*x)%p;}return res;}
+ 
+ 
+ 
+int main(){
+	fastio();
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		ll a[n];
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+		sort(a,a+n);
+		ll b[n];
+		int k=n/2,i=0,j=0;
+		if(n%2==0){
+		while(true){
+			if(i>=n/2)
+				break;
+			b[j]=a[k+i];
+			i++;
+			j++;
+			b[j]=a[k-i];
+			j++;
+		}
+	}
+	else{
+		b[0]=a[k];
+		j=1;
+		i=1;
+		while(i<=n/2){
+			b[j]=a[k-i];
+			j++;
+			b[j]=a[k+i];
+			j++;
+			i++;
+		}
+	}
+		for(int i=0;i<n;i++){
+			cout<<b[i]<<" ";
+		}
+		cout<<endl;
+ 
+	}
+}
